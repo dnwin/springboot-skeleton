86463c952704bbc256d87a08055bfcd76f12b3d0
DNwin <ndennis@gmail.com>
2017-12-10 00:09:06 -0800

    Initial skeleton

---
+++ src/main/java/com/nguyendennis/licenses/Application.java
@@ -0,0 +1,12 @@
+package com.nguyendennis.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Staring up Spring application.");
+    }
+}
